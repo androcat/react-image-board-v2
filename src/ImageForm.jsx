a66ab8bf518c5a3fe5e1fe0eaef2af8e082a9614
Jay Medina <jay.medina.celis@gmail.com>
2023-05-31 15:29:43 -0400

    Handles input

--- src/ImageForm.jsx
+++ src/ImageForm.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import "./styles/ImageFormStyles.css";
 
 export default function ImageForm() {
+  const [imgURL, setImgURL] = useState("");
+  const [imgCap, setImgCap] = useState("");
+
+  function handleURL(event) {
+    setImgURL(event.target.value);
+  }
+
+  function handleCaption(event) {
+    setImgCap(event.target.value);
+  }
+
   return (
     <form className="imgForm">
-      <label htmlFor="">Image URL</label>
-      <input type="url" name="imgURL" id="imgURL" />
-      <label htmlFor="">Image Caption</label>
-      <input type="text" name="imgCaption" id="imgCaption" />
+      <label htmlFor="imgURL">Image URL</label>
+      <input
+        type="url"
+        name="imgURL"
+        id="imgURL"
+        value={imgURL}
+        onChange={handleURL}
+      />
+      <label htmlFor="imgCaption">Image Caption</label>
+      <input
+        type="text"
+        name="imgCaption"
+        id="imgCaption"
+        value={imgCap}
+        onChange={handleCaption}
+      />
       <div className="buttons">
         {/* Add onClick to close form or reset fields to empty */}
         <button type="button" name="cancel">
