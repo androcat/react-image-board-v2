510ed086357883d43f7803f8bc66950078e05840
Jay Medina <jay.medina.celis@gmail.com>
2023-06-03 17:25:49 -0400

    Gets tid of unused code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles/App.css";
-import ImageForm from "./ImageForm";
+import ImageBoard from "./ImageBoard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Myyyyyyy Image Board :^D</h1>
-      <ImageForm />
+      <h1>Image Board</h1>
+      <ImageBoard />
     </>
   );
 }
